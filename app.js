97a954bf2f1a629e1bd596b8498dbff9de2e6816
isopha2003@gmail.com <isopha2003@gmail.com>
2026-02-01 16:19:10 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const canvas = document.querySelector("canvas"); // 도화지
 const ctx = canvas.getContext("2d"); // 붓
+const lineWidth = document.getElementById("line-width");
 
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
-const colors = [
-"#ff3838",
-"#ffb8b8",
-"#c56cf0",
-"#ff9f1a",
-"#fff200",
-"#32ff7e",
-"#7efff5",
-]
+let isPainting = false;
 
-function onClick(event) {
-    ctx.beginPath();
-    ctx.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
-    ctx.moveTo(0, 0);
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function startPainting(event) {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting)
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
